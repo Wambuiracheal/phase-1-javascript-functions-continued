a85e35b817bd548dc52f2536ec2dd42cc44a08c8
Racheal <rachealnjugush@gmail.com>
2024-09-28 22:31:40 +0300

    functions lab 2

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+//test1
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun('bathe my dog');
+
+//anonymous function use
+// const button = doucument.getElementById("button");
+// button.addEventListener("click", function (){
+//     console.log("Yet more razzling");
+// })
+
+//test2
+function mondayWork(plan = "go to the office"){
+    return `This Monday, I will ${plan}.`;
+}
+mondayWork();
+mondayWork('work from home');
+
+//test3
+function wrapAdjective(flair = "*"){
+    return function(adjective = "special"){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
